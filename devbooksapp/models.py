a174f800b381c9e639a208f18637227d8759f122
Claudio Cruz <claudiocruz1989@gmail.com>
2022-11-17 09:42:49 +0000

    Creat Category, Book, and comment databases in modals.py

--- devbooksapp/models.py
+++ devbooksapp/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.CharField(max_length=100)
+    featured_image = CloudinaryField('image', default='placeholder')
+    published_in = models.DateTimeField()
+    content = models.TextField()
+    likes = models.ManyToManyField(
+        User, related_name='book_like', blank=True)
+    favourites = models.ManyToManyField(
+        User, related_name='favourite', default=None, blank=True)
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
