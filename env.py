c2608729689130a1e76a18bf977d61320f146e83
Claudio Cruz <claudiocruz1989@gmail.com>
2022-11-29 19:01:38 +0000

    Migrate the database to elephantSQL

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://jbkiftopkgqhku:385f71ed07dfa4b52926baf0b983875d1a9cc888530766af1df19aa0912a7ea6@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d6e13vn7kfl1iq"
+os.environ["DATABASE_URL"] = "postgres://ogheyhvf:1kHs3tn4x8scDHiMl92MihRaoaBWjjHm@mouse.db.elephantsql.com/ogheyhvf"
 os.environ["SECRET_KEY"] = "devbooks2022!"
 os.environ["CLOUDINARY_URL"] = "cloudinary://416872162386444:PfAJG_eTx29nb8BAUnyTiHt3sAg@djmk0qtgj"
